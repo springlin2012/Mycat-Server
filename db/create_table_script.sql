9e965f374214308786bbbbb2bd1d6ecfcaeba7a5
moker <springlin2011@sina.com>
2016-07-30 23:07:22 +0800

    add test file.

---
+++ db/create_table_script.sql
@@ -0,0 +1,23 @@
+
+drop table if exists customer;
+
+/**用户表*/
+create table customer (
+    id int not null AUTO_INCREMENT,  -- 客户id
+		user_id varchar(32) null, 
+    name varchar(32) null,   -- 客户姓名
+		sharding int default 1,
+    constraint PK_CUSTOMER primary key (id)
+);
+
+
+drop table if exists order_info;
+
+create table order_info (
+    id int not null AUTO_INCREMENT,  -- 业务ID
+		order_id varchar(32) null,
+		user_id varchar(32) null,
+    money BIGINT not null,
+		sharding int default 1,
+    constraint PK_ORDER primary key (id)
+)
---
+++ db/init_data_script.sql
@@ -0,0 +1,10 @@
+INSERT INTO customer (user_id, name, sharding) VALUES ('db1kxufiiwjd238hf832dff1', 'db1_张三', '1');
+INSERT INTO customer (user_id, name, sharding) VALUES ('db1kxufiiwjd238hf832dff2', 'db1_李四', '1');
+INSERT INTO customer (user_id, name, sharding) VALUES ('db1kxufiiwjd238hf832dff3', 'db1_王五', '1');
+
+
+
+INSERT INTO customer (user_id, name, sharding) VALUES ('db2kxufiiwjd238hf832df1', 'db2_吴京', '2');
+INSERT INTO customer (user_id, name, sharding) VALUES ('db2kxufiiwjd238hf832dff2', 'db2_刘烨', '2');
+INSERT INTO customer (user_id, name, sharding) VALUES ('db2kxufiiwjd238hf832dff3', 'db2_六爷', '2');
+
