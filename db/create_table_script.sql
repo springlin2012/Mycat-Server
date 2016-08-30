cf272a6415cd3ffb16f2f8cb88c69c282f185ed4
lichunlin <lichunlin@xiaoniu66.com>
2016-08-30 10:46:45 +0800

    New changelist

--- db/create_table_script.sql
+++ db/create_table_script.sql
@@ -20,3 +20,37 @@ create table order_info (
 		sharding int default 1,
     constraint PK_ORDER primary key (id)
 )
+
+
+
+
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : env_develop_221
+Source Server Version : 50623
+Source Host           : 10.10.16.221:3306
+Source Database       : test_mycat
+
+Target Server Type    : MYSQL
+Target Server Version : 50623
+File Encoding         : 65001
+
+Date: 2016-07-01 18:56:17
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for sam_test
+-- ----------------------------
+DROP TABLE IF EXISTS sam_test;
+CREATE TABLE sam_test (
+  id bigint(11) NOT NULL AUTO_INCREMENT,
+  name_ varchar(255) DEFAULT NULL,
+  user_id bigint(20) DEFAULT NULL,
+  sharding bigint(255) DEFAULT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+
+
